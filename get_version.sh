0259539a2926b030c6139b244f23c6760f29cd47
mara <mara@fail.pp.ua>
2018-09-07 20:00:47 +0300

    add package l/ffmpeg

--- get_version.sh
+++ get_version.sh
@@ -11,15 +11,15 @@ get_ver() {
   pushd "$PKG_DIR" 2>&1>/dev/null
 
   if [[ -d .hg ]];then
-    printf "r%s.%s" "$(hg identify -n)" "$(hg identify -i)"
+    printf "r%s.%s\n" "$(hg identify -n)" "$(hg identify -i)"
   elif [[ -d .git ]];then
     ( set -o pipefail
 #        git describe --long 2>/dev/null | sed 's/\([^-]*-g\)/r\1/;s/-/./g' ||
-        printf "r%s.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)"
+        printf "r%s.%s\n" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)"
     )
   elif [[ -d .svn ]];then
   local ver="$(svnversion)"
-      printf "r%s" "${ver//[[:alpha:]]}"
+      printf "r%s\n" "${ver//[[:alpha:]]}"
   fi
 
   popd 2>&1>/dev/null
