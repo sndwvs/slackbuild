53f2af49d4ed65eb49566299e0bd0bc9bf66a13c
mara <mara@fail.pp.ua>
2019-08-12 22:15:17 +0300

    d: fix interpretier in jdk

---
+++ d/openjdk.bin/openjre.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export JAVA_HOME=/usr/lib/java/jre
+export MANPATH="${MANPATH}:${JAVA_HOME}/man"
+export PATH="${PATH}:${JAVA_HOME}/bin"
--- d/openjdk/openjre.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export JAVA_HOME=/usr/lib/java/jre
-export MANPATH="${MANPATH}:${JAVA_HOME}/man"
-export PATH="${PATH}:${JAVA_HOME}/bin"
