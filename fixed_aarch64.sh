554ed5b80373c57452acadc31cc09fd2ec7b4d8b
mara <mara@fail.pp.ua>
2020-05-17 02:00:56 +0300

    added script added support aarch64 in SlackBuild

---
+++ fixed_aarch64.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+fix_default() {
+    for sb in $(find . -maxdepth 3 -type f | grep .SlackBuild);do
+        psb=$(echo $sb | rev | cut -f2- -d '/' | rev)
+        pf=$(basename "$sb")
+        echo "$sb"
+        sed '0,/^elif \[ "$ARCH" = "\(x86_64\|arm.*\)" \].*$/s/^elif \[ "$ARCH" = "\(x86_64\|arm.*\)" \].*$/elif \[ \"\$ARCH\" = \"aarch64\" \]; then\
+  SLKCFLAGS=\"-O2\"\
+  LIBDIRSUFFIX=\"64\"\n&/g' -i".aarch64" "${sb}"
+    done
+}
+
+fix_default
